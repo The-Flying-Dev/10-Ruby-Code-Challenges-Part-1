561f27554227353e5aa384ca56f166159cfa6d93
The-Flying-Dev <leandranm@outlook.com>
2023-05-10 19:38:14 +0200

    Exercise 9

---
+++ 9. Return the following number from an integer passed.rb
@@ -0,0 +1,10 @@
+# Return the following number from an integer passed
+# Passes a number as an argument, then increases it by a value of one
+
+def one(n)
+    n + 1
+    #n.next
+end
+
+p one(1)
+p one(-8)
