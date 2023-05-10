b5d67e3fdf19370575231f46735661c270384aa2
The-Flying-Dev <leandranm@outlook.com>
2023-05-10 15:46:34 +0200

    Changed exercise 1 to a method

--- 1. Numbers greater than 5.rb
+++ 1. Numbers greater than 5.rb
@@ -1,15 +1,25 @@
+# Numbers greater than 5
 
 # First, you go through the entire array and ask if each number is greater than 5.
 # You then create a “count” variable by checking if each number has a value higher than 5. If it is, you count it, and if not, you don’t.
 
+def greater_than_five(arr)
 
-num_array = [17, 7, 3, 6, 10, 1]
-count = 0
+    # create a count variable
+    count = 0
 
-for i in num_array
-  if i > 5
-    count += 1
-  end 
+    # for in loop to iterate over through the array and increase the count for each element > 5
+    for i in arr
+      if i > 5
+        count += 1
+      end 
+    end
+
+    count
+
 end
 
-puts count
+
+num_array = [17, 7, 3, 6, 10, 1]
+
+puts greater_than_five(num_array)
