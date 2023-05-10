6d73dc1b715bcd9b6e3592d816a2ce2fbbdb76d0
The-Flying-Dev <leandranm@outlook.com>
2023-05-10 19:41:11 +0200

    Exercise 10

---
+++ 10. Return the sum of two numbers.rb
@@ -0,0 +1,8 @@
+# Return the sum of two numbers
+
+def addition(a, b)
+    a + b
+end 
+
+p addition(10, 12)
+p addition(2, 5)
--- 9. Return the following number from an integer passed.rb
+++ 9. Return the following number from an integer passed.rb
@@ -1,10 +1,10 @@
 # Return the following number from an integer passed
 # Passes a number as an argument, then increases it by a value of one
 
-def one(n)
+def add_one(n)
     n + 1
     #n.next
 end
 
-p one(1)
-p one(-8)
+p add_one(1)
+p add_one(-8)
